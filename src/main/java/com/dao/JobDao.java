2c9cd58a050e2e0660d13957e4ec685bc7ef0727
omsolanki271 <omsolanki271@gmail.com>
2026-03-07 23:27:42 +0530

    add new JobDao file

---
+++ src/main/java/com/dao/JobDao.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+import java.sql.Connection;
+
+public class JobDao {
+	private Connection conn;
+
+	public JobDao(Connection conn) {
+		super();
+		this.conn = conn;
+	}
+	
+	
+}
