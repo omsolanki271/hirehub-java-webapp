506d1ca847e2a16a572f87c98d7cea92e843e741
omsolanki271 <omsolanki271@gmail.com>
2026-03-08 23:59:46 +0530

    put msg using session

--- src/main/java/com/servlet/AddPostServlet.java
+++ src/main/java/com/servlet/AddPostServlet.java
@@ -6,10 +6,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.DB.DBConnect;
 import com.dao.JobDao;
 import com.entity.Jobs;
+import com.mysql.cj.Session;
 
 @WebServlet("/AddPostServlet")
 public class AddPostServlet extends HttpServlet {
@@ -36,15 +38,20 @@ public class AddPostServlet extends HttpServlet {
 		j.setStatus(status);
 		j.setDescription(dest);
 		
+		HttpSession httpSession = request.getSession();
+		
+		
 		JobDao dao = new JobDao(DBConnect.getConn());
 		boolean jobs = dao.addJobs(j);
 		if(jobs)
 		{
-			
+			httpSession.setAttribute("msg", "Job Post sucessfully..");
+			response.sendRedirect("add_job.jsp");
 		}
 		else
 		{
-			
+			httpSession.setAttribute("msg", "Something Wrong on server..");
+			response.sendRedirect("add_job.jsp");
 		}
 		
 	}
