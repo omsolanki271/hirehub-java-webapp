9eec63844272b47fbdb4030f550a606b3c15f515
omsolanki271 <omsolanki271@gmail.com>
2026-03-08 23:54:20 +0530

    fetch data to pass in AddPostServlet

--- src/main/java/com/servlet/AddPostServlet.java
+++ src/main/java/com/servlet/AddPostServlet.java
@@ -7,6 +7,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.DB.DBConnect;
+import com.dao.JobDao;
+import com.entity.Jobs;
+
 @WebServlet("/AddPostServlet")
 public class AddPostServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -17,7 +21,34 @@ public class AddPostServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		String title = request.getParameter("title");
+		String location = request.getParameter("location");
+		String category = request.getParameter("category");
+		String status = request.getParameter("status");
+		String dest = request.getParameter("description");
 
+		Jobs j = new Jobs();
+		
+		j.setTitle(title);
+		j.setLocation(location);
+		j.setCategory(category);
+		j.setStatus(status);
+		j.setDescription(dest);
+		
+		JobDao dao = new JobDao(DBConnect.getConn());
+		boolean jobs = dao.addJobs(j);
+		if(jobs)
+		{
+			
+		}
+		else
+		{
+			
+		}
+		
 	}
 
 }
+
+
